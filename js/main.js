af81e2204b81e6a14a36033f2c279491672f45a4
Callum <callum@sea-jay.net>
2021-09-22 17:14:30 +0100

    Generate exercises in index.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,30 @@ var exercise_list = document.getElementById('exercise-list');
 const num_exercises = 3;
 var exercises = [
   "Superimposed Lines",
-  "Ghosted Lines"
+  // "Ghosted Lines",
+  "Ghosted Planes",
+  "Tables of Ellipses",
+  "Funnels",
+  // "Plotted Perspective",
+  "Rough Perspective",
+  // "Rotated Boxes",
+  "Organic Perspective",
+  "Organic Arrows",
+  "Organic Forms with Contour Lines",
+  "Texture Analysis",
+  "Dissections",
+  "Form Intersections",
+  "Organic Intersections"
 ];
 
+// Generate the exercises
 if (exercises.length > 3) {
   for (var i = 0; i < num_exercises; i++) {
     var exercise_index = Math.floor(Math.random() * exercises.length);
-    var text = exercises[exercise_index];
-
-    exercises.splice(exercise_index, 1);
-
     var node = document.createElement("li");
-    var textnode = document.createTextNode(text);
+    var textnode = document.createTextNode(exercises[exercise_index]);
+
+    exercises.splice(exercise_index, 1); 
 
     node.appendChild(textnode);
     exercise_list.appendChild(node);
